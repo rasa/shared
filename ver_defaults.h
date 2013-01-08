34c109e29438dbdf922cf8ae67c58de32d00bddc
Ross Smith II <ross@smithii.com>
2013-01-07 17:15:43 -0800

    Adding missing and updated files

--- ver_defaults.h
+++ ver_defaults.h
@@ -2,7 +2,7 @@
 
 $Id$
 
-Copyright (c) 2002-2011 Ross Smith II (http://smithii.com). All rights reserved.
+Copyright (c) 2002-2013 Ross Smith II (http://smithii.com). All rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of version 2 of the GNU General Public License
@@ -30,7 +30,7 @@ General Public License for more details.
 #endif
 
 #ifndef VER_LEGAL_COPYRIGHT
-#define VER_LEGAL_COPYRIGHT		"Copyright (c) 2005-2011 Ross Smith II. All Rights Reserved."
+#define VER_LEGAL_COPYRIGHT		"Copyright (c) 2005-2013 Ross Smith II. All Rights Reserved."
 #endif
 
 #ifndef VER_COMMENTS
